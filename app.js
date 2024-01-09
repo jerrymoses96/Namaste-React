279ad7f20177dae8dbbf55f3a719e50cca67b5b6
jerin <jerinkunnamkada@gmail.com>
2024-01-09 14:36:24 +0530

    second class

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [React.createElement(
-    "div",
-    { id: "child1" },
-    [React.createElement("h1", {}, "hello from React h1"),
-    React.createElement("h2", {}, "hello from React h2")]
-  ),React.createElement(
-    "div",
-    { id: "child2" },
-    [React.createElement("h1", {}, "hello from React h1"),
-    React.createElement("h2", {}, "hello from React h2")]
-  )]
-);
+import React from "react";
+import ReactDOM from "react-dom";
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "hello from React h1"),
+    React.createElement("h2", {}, "am jerin ouseph"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "hello from React h1"),
+    React.createElement("h2", {}, "hello from React h2"),
+  ]),
+]);
 
 console.log(parent);
 
