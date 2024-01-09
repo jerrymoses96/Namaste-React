bc0b351a5cc0e4514091c891dc2d2c77f6720a4e
jerin <jerinkunnamkada@gmail.com>
2024-01-09 09:25:34 +0530

    first episode

---
+++ app.js
@@ -0,0 +1,21 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement(
+    "div",
+    { id: "child1" },
+    [React.createElement("h1", {}, "hello from React h1"),
+    React.createElement("h2", {}, "hello from React h2")]
+  ),React.createElement(
+    "div",
+    { id: "child2" },
+    [React.createElement("h1", {}, "hello from React h1"),
+    React.createElement("h2", {}, "hello from React h2")]
+  )]
+);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
